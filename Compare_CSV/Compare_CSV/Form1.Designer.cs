03a750100e81abd69ecaed3dcde1705321c29fe4
mephu <mephirr7@gmail.com>
2018-07-08 20:08:40 +0200

    dodane komentarze

--- Compare_CSV/Compare_CSV/Form1.Designer.cs
+++ Compare_CSV/Compare_CSV/Form1.Designer.cs
@@ -31,23 +31,31 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 39);
+            this.button1.Location = new System.Drawing.Point(9, 25);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 58);
+            this.button1.Size = new System.Drawing.Size(79, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "Wczytaj pliki";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,12 +63,14 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(531, 116);
+            this.button2.Location = new System.Drawing.Point(687, 185);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 39);
+            this.button2.Size = new System.Drawing.Size(67, 46);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Zapis do pliku";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // dataGridView1
             // 
@@ -81,7 +91,8 @@
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 202);
+            this.dataGridView1.Location = new System.Drawing.Point(8, 246);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
@@ -92,32 +103,15 @@
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(513, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(371, 331);
             this.dataGridView1.TabIndex = 2;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(531, 301);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 38);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(531, 183);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 36);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(496, 39);
+            this.button5.Location = new System.Drawing.Point(687, 77);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(101, 39);
+            this.button5.Size = new System.Drawing.Size(67, 25);
             this.button5.TabIndex = 5;
             this.button5.Text = "Porównaj";
             this.button5.UseVisualStyleBackColor = true;
@@ -125,42 +119,116 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(157, 39);
+            this.textBox1.Location = new System.Drawing.Point(105, 25);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(267, 26);
+            this.textBox1.Size = new System.Drawing.Size(293, 20);
             this.textBox1.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(157, 71);
+            this.textBox2.Location = new System.Drawing.Point(105, 46);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(267, 26);
+            this.textBox2.Size = new System.Drawing.Size(293, 20);
             this.textBox2.TabIndex = 7;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(13, 116);
+            this.textBox3.Location = new System.Drawing.Point(9, 75);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(512, 26);
+            this.textBox3.Size = new System.Drawing.Size(389, 20);
             this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(11, 211);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(368, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(383, 211);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(266, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // dataGridView2
+            // 
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView2.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView2.Location = new System.Drawing.Point(383, 246);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(371, 331);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(403, 8);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 139);
+            this.checkedListBox1.TabIndex = 9;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(529, 8);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(120, 139);
+            this.checkedListBox2.TabIndex = 9;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 364);
+            this.ClientSize = new System.Drawing.Size(765, 588);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,12 +239,15 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
     }
 }
 
--- Compare_CSV/Compare_CSV/Form1.cs
+++ Compare_CSV/Compare_CSV/Form1.cs
@@ -2,6 +2,7 @@
 using System.Windows.Forms;
 using System.IO;
 using System.Collections.Generic;   //potrzebne do list<string>
+using System.Linq;
 
 namespace WindowsFormsApp1
 {
@@ -13,10 +14,18 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        public string filename { get; set; }
+        public string filename1 { get; set; }
         public string filename2 { get; set; }
         public string columns_ff { get; set; }
         public string columns_sf { get; set; }
+        char[] delimiters = {';', ',', '.', ' '};
+        char delimiter1 = ' ';
+        char delimiter2 = ' ';
+        string[] columns;
+        List<string> listA = new List<string>();
+        List<string> listB = new List<string>();
+        public string[] lineBuffer { get; set; }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -24,52 +33,111 @@ namespace WindowsFormsApp1
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
             openFileDialog1.Filter = "CSV files|*.csv|All files|*.*";
             openFileDialog1.Title = "Wybierz pliki do porownania";
-
-
+            int countd = 0;
+            int highestdel = 0;
+            //OPEN BOTH FILES
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                filename = openFileDialog1.FileName;
-                textBox1.Text = filename;
-                using (var reader = new StreamReader(filename))
+                filename1 = openFileDialog1.FileName;
+                textBox1.Text = filename1;
+                using (var reader = new StreamReader(filename1))
                 {
+                    // OPEN FILE AND CHECK WHICH DELIMITER IS USED
                     columns_ff = reader.ReadLine();
-                    textBox3.Text = columns_ff;
+                    columns_ff.Trim();
+                    foreach (var item in delimiters)
+                    {
+                        countd = columns_ff.Split(item).Length - 1; //splituje na itemy oddzielone charem -1 bo liczy od 0
+                        
+                        if (countd > highestdel)
+                        {
+                            highestdel = countd;
+                            delimiter1 = item;
+                        } 
+                    }
+                    //ADD COLUMN NAMES TO CHECKLIST BOX
+                    columns = columns_ff.Split(delimiter1);
+                    foreach (var item1 in columns)
+                    {
+                        checkedListBox1.Items.Add(item1);
+                        //dataGridView1.Columns.Add(item1, item1);    //przypisuje nazwy kolumn
+                    }
+                    //ADD VALUES TO PROPER LISTS
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        //listA.Add(line);
+                        lineBuffer = line.Split(delimiter1);
+                        Results.
+                        //dataGridView1.Rows.Add(line.Split(delimiter1));  //dodaje wiersze 
+                    }
+                    
+                    textBox3.Text = "Pierwszy delimiter to: "+delimiter1;
                 }
                     if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
                     {
                         filename2 = openFileDialog1.FileName;
                         textBox2.Text = filename2;
+                    using (var reader = new StreamReader(filename2))
+                    {
+                        columns_ff = reader.ReadLine();
+                        columns_ff.Trim();
+                        foreach (var item in delimiters)
+                        {
+                            highestdel = 0;
+                            countd = columns_ff.Split(item).Length - 1; //splituje na itemy oddzielone charem -1 bo liczy od 0
+                            if (countd > highestdel)
+                            {
+                                highestdel = countd;
+                                delimiter2 = item;
+                            }
+                        }
+                        columns = columns_ff.Split(delimiter2);
+                        foreach (var item1 in columns)
+                        {
+                            checkedListBox2.Items.Add(item1);
+                            //dataGridView2.Columns.Add(item1, item1);  //dodaje kolumny
+                        }
+                        while (!reader.EndOfStream)
+                        {
+                            var line = reader.ReadLine();
+                            listB.Add(line);
+                            //dataGridView2.Rows.Add(line.Split(delimiter2));   //dodaje wiersze
+                        }
+                        textBox3.Text = "Pierwszy delimiter to: " + delimiter1 + "Drugi delimiter to: " + delimiter2;
+
                     }
-
-            }
-        }
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Compare();
-        }
-
-        public void Compare()
-        {
-            using (var reader = new StreamReader(filename))
-            {
-                List<string> listA = new List<string>();
-                List<string> listB = new List<string>();
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    var values = line.Split(';');
-
-                    listA.Add(values[0]);
-                    listB.Add(values[1]);
                 }
+
             }
         }
+        private void button5_Click(object sender, EventArgs e) //porownaj
+        {
+            dataGridView1.Columns.Clear();  //wyczysc grid aby sie nie dopisywal
+            dataGridView2.Columns.Clear();
+            foreach (int index in checkedListBox1.CheckedIndices)
+            {
+                dataGridView1.Columns.Add(columns[index], columns[index]);
+                foreach (var item in listA)
+                {
+                    item.Split(delimiter1);
+                    dataGridView1.Rows.Add(item[index]);
+                }
+                
+            }
+            foreach (int index in checkedListBox2.CheckedIndices)
+            {
+                dataGridView2.Columns.Add(columns[index], columns[index]);
+            }
 
-        private void button5_Click(object sender, EventArgs e)
+          
+
+        }
+        private void button2_Click_1(object sender, EventArgs e)
         {
 
         }
-
-
     }
+
+   
 }
